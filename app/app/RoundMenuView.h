580c9fdda153f9f5b2a0d0e18914ccda491122fd
Paul de Lange <paul@paul.home>
2012-07-22 17:17:32 +0200

    work

---
+++ app/app/RoundMenuView.h
@@ -0,0 +1,13 @@
+//
+//  RoundMenuView.h
+//  app
+//
+//  Created by Paul de Lange on 22/07/12.
+//  Copyright (c) 2012 Scimob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoundMenuView : UIView
+
+@end
---
+++ app/app/RoundMenuView.m
@@ -0,0 +1,31 @@
+//
+//  RoundMenuView.m
+//  app
+//
+//  Created by Paul de Lange on 22/07/12.
+//  Copyright (c) 2012 Scimob. All rights reserved.
+//
+
+#import "RoundMenuView.h"
+
+@implementation RoundMenuView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
